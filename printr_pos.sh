737071e59d754d4395b52222868a89ab07634cbd
Christopher Bero <bigbero@gmail.com>
2015-05-27 17:40:14 +0000

    updated URL to match authbind use with octoprint

--- printr_pos.sh
+++ printr_pos.sh
@@ -2,7 +2,7 @@
 
 # This script will report the location of the printer's hotend
 
-RESPONSE=`curl -H "X-Api-Key: $OCTO_API_KEY" -F command=M114 "bns-daedalus.256.makerslocal.org:8081/api/printer/command"`
+RESPONSE=`curl -H "X-Api-Key: $OCTO_API_KEY" -F command=M114 "bns-daedalus.256.makerslocal.org/api/printer/command"`
 
 echo ${RESPONSE}
 
