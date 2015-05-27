4cf2fcf540723f957c298ba56ff82a9da4c00e7a
Christopher Bero <csb0019@uah.edu>
2015-05-27 03:14:49 -0500

    added rq message to pop_part

--- pop_part.sh
+++ pop_part.sh
@@ -49,18 +49,32 @@ fi
 echo "`date`: Printer looks good, proceeding to loosen part" >> $LOG
 
 function cycle_hotbed {
+
 # Run the hotbed code
 #curl -H "X-Api-Key: $api_key" -F select=true -F print=false -F file=@$f http://beaglebone.local:5000/api/files/local
 #API_HOTBED=`curl -H "X-Api-Key: $OCTO_API_KEY" -F select=true -F print=true -F file=@gcode/cycle_hotbed.gcode  http://localhost:8081/api/files/local -o /tmp/printr_upload.json`
+
 POST /printer/command '{"command":"M140 S70"}'
 sleep 5m
 /bin/bash fanctl.sh on
-sleep 10m
-POST /printer/command '{"command":"M140 S0"}'
-sleep 10m
-/bin/bash fanctl.sh off
 sleep 5m
+POST /printer/command '{"command":"M140 S0"}'
+sleep 20m
+/bin/bash fanctl.sh off
+sleep 3m
 echo "`date`: Done with cycle." >> $LOG
+
 }
 
-cycle_hotbed
+#cycle_hotbed
+#cycle_hotbed
+#cycle_hotbed
+#cycle_hotbed
+
+resty https://crump.space/rq
+
+MSG="Finished automatic buildplate cycling. RQ API via resty."
+
+#curl --data "{\"message\":\"${MSG}\", \"channel\":\"##rqtest\", \"isaction\":false, \"key\":\"${APIKEY}\"}" https://crump.space/rq/relay -H "Content-Type:application/json"
+POST /relay "{\"message\":\"${MSG}\", \"channel\":\"##rqtest\", \"isaction\":false, \"key\":\"12345\"}"
+
