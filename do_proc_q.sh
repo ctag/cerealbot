0d29d0055b9ec1857d616f210babc5c315eba097
Christopher Bero <bigbero@gmail.com>
2015-06-10 21:07:39 -0500

    Added crontab queue processor!

--- do_proc_q.sh
+++ do_proc_q.sh
@@ -10,12 +10,24 @@ fi
 printr_status
 STATUS=$?
 
+function check_status {
 if [ $STATUS -ne 0 ]; then
         write_msg "LOG,STD" "Printer status is non-zero. Exiting $0." "$LOG"
         exit
 fi
+if [ $CB_BUSY = "true" ]; then
+        write_msg "LOG,STD" "CB_BUSY variable is set. Exiting $0." "$LOG"
+	exit
+fi
+}
 
-#ret="GET /files | grep name | awk '{ print $2 }' | sed s/,//"
+check_status
+
+sleep 5m
+
+check_status
+
+write_msg "LOG,STD" "Printer appears to be free, looking for something to print." "$LOG"
 
 . resty
 
@@ -23,11 +35,11 @@ resty http://localhost/api >> /dev/null
 
 GET /files/local | grep name | awk '{ print $2 }' | sed s/,// | sed s/\"// | sed s/\"// | while read -r file
 do
-echo "file: $file"
+write_msg "LOG,STD" "Checking file: $file" "$LOG"
 file_status=`GET "/files/local/$file"`
 echo $file_status | grep '"failure": 0' | grep '"success": 0' >> /dev/null
 if [ $? -eq 0 ]; then
-  echo "File hasn't been printed yet: $file"
+  write_msg "LOG,STD" "Printing new file: $file" "$LOG"
   POST "/files/local/$file" '{"command":"select","print":"true"}'
   exit
 fi
