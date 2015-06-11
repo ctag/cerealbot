38747cd4d6f9308eee89c0210dcca6df36ede2b7
Christopher Bero <bigbero@gmail.com>
2015-06-11 10:18:01 -0500

    fixed queue processor

--- do_proc_q.sh
+++ do_proc_q.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 LOG=/tmp/print_new.log
 
@@ -7,16 +8,16 @@ if [ ! -d "$CB_DIR" ]; then
 fi
 . $CB_DIR/util.sh
 
-printr_status
-STATUS=$?
 
 function check_status {
+printr_status
+STATUS=$?
 if [ $STATUS -ne 0 ]; then
-        write_msg "LOG,STD" "Printer status is non-zero. Exiting $0." "$LOG"
+        write_msg "LOG,STD,RQ" "Printer status is non-zero. Exiting $0." "$LOG"
         exit
 fi
 if [ $CB_BUSY = "true" ]; then
-        write_msg "LOG,STD" "CB_BUSY variable is set. Exiting $0." "$LOG"
+        write_msg "LOG,STD,RQ" "CB_BUSY variable is set. Exiting $0." "$LOG"
 	exit
 fi
 }
@@ -27,7 +28,7 @@ sleep 5m
 
 check_status
 
-write_msg "LOG,STD" "Printer appears to be free, looking for something to print." "$LOG"
+write_msg "LOG,STD,RQ" "Printer appears to be free, looking for something to print." "$LOG"
 
 . resty
 
@@ -41,10 +42,12 @@ echo $file_status | grep '"failure": 0' | grep '"success": 0' >> /dev/null
 if [ $? -eq 0 ]; then
   write_msg "LOG,STD" "Printing new file: $file" "$LOG"
   POST "/files/local/$file" '{"command":"select","print":"true"}'
+  write_msg "RQ" "Oh look, a new file. I suppose [$file] can be processed.."
   exit
 fi
 done
 
+write_msg "RQ" "I am diligent, but the queue is empty."
 
 
 
